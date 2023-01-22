2768151cf0bf9355d07a210898b4d560705f270f
Khushi Prasad <khushi@Khushis-MacBook-Air-3.local>
2023-01-22 21:01:07 +0530

    project added

---
+++ main.py
@@ -0,0 +1,143 @@
+# Importing the required packages
+import streamlit as st
+import pandas as pd
+import math as m
+import smtplib, csv
+from email.message import EmailMessage
+
+sender = "khushi01.projects@gmail.com"
+password = "eafedgdmebqwlwax"
+
+def topsis(inputFileName, weights, impacts):
+
+    # Reading the input csv file
+    dataset = pd.read_csv(inputFileName)
+
+    # Dropping and NULL values
+    dataset.dropna(inplace = True)
+
+    # Dropping categorical values and using only numerical values
+    df = dataset.iloc[0:,1:].values
+
+    # Converting the dataset into a matrix
+    mat = pd.DataFrame(df)
+
+    # Calculating root of sum of square for each column
+    rootOfSumOfSquares = []
+    for col in range(0, len(mat.columns)):
+        sum = 0
+        colValues = mat.iloc[0:,[col]].values
+        for val in colValues:
+            sum = sum + m.pow(val,2)
+        rootOfSumOfSquares.append(m.sqrt(sum))
+    
+    # Dividing every matrix value with its corresponding column rootOfSumOfSquares value to get Normalized Performance Value
+    k = 0
+    while(k<len(mat.columns)):
+        for j in range(0,len(mat)):
+            mat[k][j] = mat[k][j]/rootOfSumOfSquares[k]
+        k = k+1
+
+    # Multiplying each of the matrix value with its corresponding column weight to get Weighted Normalized Decision Matrix
+    k = 0
+    while(k<len(mat.columns)):
+        for j in range(0,len(mat)):
+            mat[k][j] = mat[k][j]*weights[k]
+        k = k+1
+
+    # Finding the ideal best and worst values for each column according to the impact of that column
+    idealBestValue = []
+    idealWorstValue = []
+    for col in range(0, len(mat.columns)):
+        colValues = mat.iloc[0:,[col]].values
+
+        if impacts[col]=='+':
+            maxVal = max(colValues)
+            minVal = min(colValues)
+            idealBestValue.append(maxVal)
+            idealWorstValue.append(minVal)
+
+        if impacts[col]=='-':
+            maxVal = max(colValues)
+            minVal = min(colValues)
+            idealBestValue.append(minVal)
+            idealWorstValue.append(maxVal)
+
+    # Calculating the Euclidean Distance from ideal best and ideal worst values for each row     
+    SiPlus = []
+    SiMinus = []
+    for row in range(0, len(mat)):
+        rowValues = mat.iloc[row, 0:].values
+        temp1 = 0
+        temp2 = 0
+        for val in range(0, len(rowValues)):
+            temp1 = temp1 + m.pow(rowValues[val]-idealBestValue[val],2)
+            temp2 = temp2 + m.pow(rowValues[val]-idealWorstValue[val],2)
+        SiPlus.append(m.sqrt(temp1))
+        SiMinus.append(m.sqrt(temp2))
+
+    # Calculating the Performance Score for each row
+    Pi = []
+    for row in range(0, len(mat)):
+        Pi.append(SiMinus[row]/(SiMinus[row]+SiPlus[row]))
+
+    # Assigning Rank
+    Rank = []
+    sortPi = sorted(Pi, reverse=True)
+    for row in range(0, len(mat)):
+        for i in range(0, len(sortPi)):
+            if Pi[row] == sortPi[i]:
+                Rank.append(i+1)
+
+    # Appending the new columns to original matrix
+    col1 = dataset.iloc[:,[0]].values
+    mat.insert(0, dataset.columns[0], col1)
+    mat['TOPSIS Score'] = Pi
+    mat['Rank'] = Rank
+
+    return mat
+
+with st.form("form1", clear_on_submit=True):
+    inputFileName = st.file_uploader("Input File Name")
+    weights = st.text_input("Weights")
+    impacts = st.text_input("Impacts")
+    email = st.text_input("Email ID")
+    submit = st.form_submit_button("Submit")
+
+subject = "Results"
+msg = EmailMessage()
+msg['Subject'] = subject
+msg['From'] = sender
+msg['To'] = email
+filename = "results.csv"
+if submit is True:
+    weights = weights.split(',')
+    for i in range(0, len(weights)):
+            weights[i] = int(weights[i])
+    impacts = impacts.split(',')
+    csvfile = topsis(inputFileName ,weights, impacts)       
+
+    with open(filename, 'w') as csvfiles: 
+        # creating a csv writer object 
+        csvwriter = csv.writer(csvfiles) 
+            
+        # writing the fields 
+        csvwriter.writerow(list(csvfile.columns)) 
+            
+        # writing the data rows 
+        file_data = csvwriter.writerows(list(csvfile.values.tolist()))
+    
+    with open(filename,"rb") as f:
+            file_data = f.read()
+            file_name = f.name
+            msg.add_attachment(file_data,maintype="application",subtype="csv",filename=file_name)
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            try:
+                server.login(sender, password)
+                print("Logged In..")
+                server.send_message(msg)
+                print("Email Sent!")
+            except smtplib.SMTPAuthenticationError:
+                print("Unable to sign in")
